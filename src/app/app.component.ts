6e46fda746973d7f56ba9156aeb63827fc261f9a
Eddy Soma <esoma92.dev@gmail.com>
2019-03-18 18:26:00 -0400

    remove boilerplate templating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'koality-harmonia-portal';
+  title = 'Koality Harmonia';
 }
