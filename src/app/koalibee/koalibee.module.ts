6777dae0664a9a8b7993d2df5f693e27874e141a
Eddy Soma <esoma92.dev@gmail.com>
2019-03-19 15:20:41 -0400

    add koalibee, admin, and shared module

---
+++ src/app/koalibee/koalibee.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { KoalibeeRoutingModule } from './koalibee-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    KoalibeeRoutingModule
+  ]
+})
+export class KoalibeeModule { }
