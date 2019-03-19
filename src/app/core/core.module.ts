f4ad2a816d7ff59be3f7e9a06cd5bd4d3397d2b8
Eddy Soma <esoma92.dev@gmail.com>
2019-03-18 20:03:28 -0400

    add css libs and created a sample landing page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoreRoutingModule } from './core-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    CoreRoutingModule,
+    MaterialModule
+  ],
+  exports: [
+    RouterModule,
+    MaterialModule,
+    NgbModule
   ]
 })
 export class CoreModule { }
