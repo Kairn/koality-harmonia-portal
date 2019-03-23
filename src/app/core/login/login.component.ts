5cc6cb67828a7fc043b1019dac45e0a3bae983af
Eddy Soma <esoma92.dev@gmail.com>
2019-03-23 16:29:36 -0400

    build the login page - added global scss variables file - added markups and styles to the login page - added form control with validations to the login form

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
   }
 
 }
