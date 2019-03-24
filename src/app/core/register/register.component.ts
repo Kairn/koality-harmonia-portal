3044986b2dab6e4c67e0867b607e93312af8c6b5
Eddy Soma <esoma92.dev@gmail.com>
2019-03-24 13:19:15 -0400

    write markups to register page

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(
+    public fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmPassword: ['', Validators.required],
+      terms: [false, Validators.requiredTrue]
+    });
   }
 
+  passwordsMatched(): boolean {
+    const ps = this.registerForm.get('password').value;
+    const psc = this.registerForm.get('confirmPassword').value;
+
+    if (ps == null || psc == null) {
+      return false;
+    }
+    if (ps.toString().length < 1 || psc.toString().length < 1) {
+      return false;
+    } else {
+      return ps.toString() === psc.toString();
+    }
+  }
 }
