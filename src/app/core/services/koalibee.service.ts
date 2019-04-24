9b1db866e1efc4e11ff324222b25ef10847d6839
Eddy Soma <esoma92.dev@gmail.com>
2019-04-23 23:17:55 -0500

    add post moment service method

--- src/app/core/services/koalibee.service.ts
+++ src/app/core/services/koalibee.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpResponse,
+  HttpHeaders
+} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+import { Moment } from 'src/app/shared/models/moment';
+import { Review } from 'src/app/shared/models/review';
+import { Track } from 'src/app/shared/models/track';
+import { Album } from 'src/app/shared/models/album';
+import { Koalibee } from 'src/app/shared/models/koalibee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KoalibeeService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient,
+    public router: Router,
+    public as: AuthService
+  ) { }
+
+  postMoment(momentData: string): Observable<HttpResponse<string>> {
+    return this.http.post<string>(
+      AuthService.baseUrl + 'moment/' + 'post/' + this.as.getKoalibeeId(),
+      momentData,
+      {
+        observe: 'response',
+        responseType: 'text' as 'json',
+        headers: new HttpHeaders({
+          'Auth-Token': localStorage.getItem('Auth-Token')
+        })
+      }
+    );
+  }
+
 }
